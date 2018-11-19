2cfcdf48626701a3441647feacdd0338bfb980a6
Cristian <cristian.budzicz@gmail.com>
2018-11-19 17:57:46 -0300

    prueba de ngmodel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  contador = 1;
-
-  incrementar() {
-    this.contador++;
-  }
-
-  decrementar() {
-    this.contador--;
-  }
+  nombre='';
+  apellido='';
 
 }
