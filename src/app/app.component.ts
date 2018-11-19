e4bef9a080b3002a114d558508df507d5282bd83
Cristian <cristian.budzicz@gmail.com>
2018-11-19 16:58:46 -0300

    prueba evento click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-// title = ' mi app';
-nombre = 'Rodriguez Pablo';
-  edad = 18;
-  email = 'rpablo@gmail.com';
-  sueldos = [1700, 1600, 1900];
-  activo = true;
+  contador = 1;
 
-  esActivo() {
-    if (this.activo)
-      return 'Trabajador Activo';
-    else
-      return 'Trabajador Inactivo';
+  incrementar() {
+    this.contador++;
   }
 
-  ultimos3Sueldos() {
-    let suma=0;
-    for(let x=0; x<this.sueldos.length; x++)
-      suma+=this.sueldos[x];
-    return suma;
+  decrementar() {
+    this.contador--;
   }
+
 }
